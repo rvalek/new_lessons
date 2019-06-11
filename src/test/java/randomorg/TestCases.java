fe0077f3ceb3d9d7c4661c7af9c5a9e0d290b410
Robert Valek <rvalek@intersog.com>
2019-06-12 00:05:19 +0300

    Added one more test for RandomOrg

--- src/test/java/randomorg/TestCases.java
+++ src/test/java/randomorg/TestCases.java
@@ -1,5 +1,9 @@
 package randomorg;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,6 +12,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestCases {
@@ -38,23 +43,43 @@ public class TestCases {
     }
 
     @Test
-    public static void optionFields() {
-        findAndFill(By.cssSelector("input[name='num']") , "1");
-        findAndFill(By.cssSelector("input[name='min']") , "0");
-        findAndFill(By.cssSelector("input[name='max']") , "9");
-        findAndFill(By.cssSelector("input[name='col']") , "1");
-
-
-
-    }
-
-    @Test
     public static void generateOnHomePage() {
         browser.switchTo().frame(browser.findElement(By.cssSelector("iframe")));
         browser.findElement(By.cssSelector("#true-random-integer-generator-button")).click();
+
+        // Add assertion
     }
 
-    public static WebElement findAndFill(By locator, String text) {
+    @DataProvider
+    public static Object[][] dataProvider() {
+        return new Object[][] { { 0, 9, 100, 10 }, { 0, 100, 10000, 1 }, };
+    }
+
+    @Test(dataProvider = "dataProvider")
+    public static void generateNumber(Integer min, Integer max, Integer total, Integer threshold) {
+        findAndFill(By.cssSelector("input[name=num]"), total.toString());
+        findAndFill(By.cssSelector("input[name=min]"), min.toString());
+        findAndFill(By.cssSelector("input[name=max]"), max.toString()).submit();
+
+        String[] data = browser.findElement(By.cssSelector("pre.data")).getText().trim().split("\\s+");
+        HashMap<String, Integer> numberCounter = new HashMap<>();
+
+        // int sequenceLength = 3;
+        // String temp = "";
+        for (String n : data) {
+            // temp += n;
+
+            // if(temp.length() == sequenceLength) {
+            numberCounter.put(n, numberCounter.containsKey(n) ? numberCounter.get(n) + 1 : 1);
+            // temp = "";
+            // }
+        }
+
+        Collection<Integer> occurances = numberCounter.values();
+        Assert.assertTrue(Collections.max(occurances) - Collections.min(occurances) <= (total / 100 * threshold));
+    }
+
+    private static WebElement findAndFill(By locator, String text) {
         WebElement e = browser.findElement(locator);
         e.clear();
         e.sendKeys(text);
